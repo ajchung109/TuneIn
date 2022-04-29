c359063b2ce4934645dd234996af7a9f231fb211
Anna Chung <ajchung@berkeley.edu>
2022-04-29 00:32:58 -0700

    singled out spotify url link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import React from 'react'; 
-// import {makeStyles, createStyles, Theme} from '@material-ui/core/styles'; 
 import day from './day'; 
 import "antd/dist/antd.css";
 import { Card } from 'antd';
 import { Row, Col } from 'antd';
+
+
+
+
 const { Meta } = Card;
 
-//import ReactDOM from 'react-dom'; 
 
 
 function App() {
-
+  
   var day = [
     { "name": "Monday", "image": "https://www.artnews.com/wp-content/uploads/2020/04/strokes.jpg?w=682", "song": "DojaCat - Say So"},
     { "name": "Tuesday", "image": "https://www.artnews.com/wp-content/uploads/2020/04/71bOXHJKHL._SL1400_-1.jpg", "song": "Pharrell Williams - Happy"}, 
@@ -39,9 +41,8 @@ function App() {
           TuneIn
         </h1>
       <header className="App-header">
-        <p> 
-          Song of the Day: Link to Song Here???
-        </p>
+        <div id = "newsong">test
+        </div>
         <Row gutter={16}>
           {element}
         </Row>
@@ -50,23 +51,27 @@ function App() {
           <day /> 
         </div> 
         <input id ="feeling" />
+        <p>this.state.song</p>
         <button onClick = {genSongClicked}>New Song!</button>
       </header>
     </div>
   );
 }
 
-const token = 'BQCXfFqFgS5ThU64ojj7VUV5nrc1y2jKjylb64XsJoSkJXHlRKRRuh517GqIFrMRvEKbp_MiFl_gjPwVZuuT0CAI3ipOQ72qHERdoDYxy1xd9IitqilcY0sF-LYs_BUmOs18SGfn1tdMa-_UQv7pgXnwcMpla-fjVLbcj18r';
+const token = 'BQDw7PECV2LmfpJTJkTCGRrk-w8nFpquCVCuYTMAK5NA66e2KwrMRfbKUKmr6G08eVOvzkgTGppPDULVfuQNiHOrDI6fHbwSIIFWK97MKQZPpJFSMhZZMysxxMiyPzm_iRUG0_yjIJaSlfTwF0zR5ics_pbqWgA9nk6yQ7ORTYX1nZmo-6_ZVXzMehk';
 
 function genSongClicked() {
   const dayToday = checkDay();
-  if (dayToday != false){
-    const word = document.getElementById('feeling').value;
-    genSong(word, dayToday);
-  } else {
-    alert("A song has already been generated for today.")
-  }
-}
+  const word = document.getElementById('feeling').value;
+  genSong(word,dayToday); 
+ 
+  // if (dayToday != false){
+  //   const word = document.getElementById('feeling').value;
+  //   genSong(word, dayToday);
+  // } else {
+  //   alert("A song has already been generated for today.")
+  // }
+ }
 
 function checkDay() { 
   const dayToday = new Date().toLocaleString('default', {weekday: 'long'});
@@ -77,19 +82,51 @@ function checkDay() {
 }
 
 function genSong(word, dayToday) {
+  fetch('https://api.spotify.com/v1/search?q=' + word + '&type=track&limit=10', {
+      method: 'GET',
+      headers: {'Authorization' : 'Bearer ' + token}
+    })
+    .then(response => response.json())
+    .then((data) => {
+      
+       console.log(data.tracks.items[0].external_urls.spotify);
+      
+    });
+  // data.tracks.items[0].external_urls.spotify
   // document.cookie = dayToday + "=starships"
   // console.log(document.cookie)
   // return
-  fetch('https://api.spotify.com/v1/search?q=' + word + '&type=track&limit=10', {
-    method: 'GET',
-    headers: {'Authorization' : 'Bearer ' + token}
-  })
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data)
-    const choice = Math.floor(Math.random() * data.tracks.items.length);
-    document.cookie = dayToday + "=" + data.tracks.items[choice].name;
-    });
+  // const fetchSong = async() => {
+  //   const response = await fetch('https://api.spotify.com/v1/search?q=' + word + '&type=track&limit=10', {
+  //     method: 'GET',
+  //     headers: {'Authorization' : 'Bearer ' + token}
+  //   })
+  //   .then(response => response.json())
+  //   .then((data) => {
+      
+  //      return data.tracks.items[0].external_urls.spotify;
+  //   });
+  //   const {song} = await response.json(); 
+  //   this.setState({
+  //     song 
+  //   }); 
+    
+
+  // }; 
+  // fetchSong(); 
+  
+  
+
+    // const choice = Math.floor(Math.random() * data.tracks.items.length);
+    // document.cookie = dayToday + "=" + data.tracks.items[choice].name;
+    // const newDiv = document.createElement("div"); 
+    // const newTag = '<a href= ' + song + '>Click Me</a>'; 
+    // newDiv.innerHTML = newTag;   
+    // const currentDiv = document.getElementById("newsong"); 
+    // let container = document.querySelector('div'); 
+    // let head = container.querySelector('header'); 
+    // container.insertBefore(newDiv,currentDiv ); 
+    
   }
 
 
