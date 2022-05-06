94edd5ef1780c8bfa653b203e53b4109c108c930
Anna Chung <ajchung@berkeley.edu>
2022-05-06 09:10:34 -0700

    working version?

--- src/App.js
+++ src/App.js
@@ -9,49 +9,119 @@ import { useEffect, useState } from "react";
 
 const { Meta } = Card;
 
-
-var x = ""
-var y = ""
-
-  // var day = [
-  //   {"name": "Monday", "image": "https://www.artnews.com/wp-content/uploads/2020/04/strokes.jpg?w=682", "song": "DojaCat - Say So", "songlink": "https://www.youtube.com/watch?v=QYh6mYIJG2Y"},
-  //   { "name": "Tuesday", "image": "", "song": "", "songlink": ""}, 
-  //   { "name": "Wednesday", "image": "", "song": "", "songlink": ""}, 
-  //   { "name": "Thursday", "image": "", "song": "", "songlink": ""},
-  //   { "name": "Friday", "image": "", "song": "", "songlink": ""}
-  // ]
-
-function App() {
   
+function App() {
+  // document.cookie = "Thursday=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   var day = [
-    { "name": "Monday", "image": "https://www.artnews.com/wp-content/uploads/2020/04/strokes.jpg?w=682", "song": "The Strokes - The Adults are Talking", "songlink": "https://open.spotify.com/album/2xkZV2Hl1Omi8rk2D7t5lN?highlight=spotify:track:5ruzrDWcT0vuJIOMW7gMnW"},
-    { "name": "Tuesday", "image": "https://i1.sndcdn.com/artworks-000069755023-k1kc7q-t500x500.jpg", "song": "Pharrell Williams - Happy", "songlink": "https://open.spotify.com/album/0lrmy4pJINsFzycJvttX2W"}, 
-    { "name": "Wednesday", "image": "https://images.surfacemag.com/app/uploads/2022/03/29123410/harry-styles-harrys-house.jpg", "song": "Harry Styles - As It Was", "songlink": "https://wwwhttps://open.spotify.com/album/2pqdSWeJVsXAhHFuVLzuA8.youtube.com/watch?v=QYh6mYIJG2Y"}, 
-    { "name": "Thursday", "image": "https://m.media-amazon.com/images/I/71o97DFZzdL._SS500_.jpg", "song": "Katy Perry - Part of Me", "songlink": "https://open.spotify.com/track/1nZzRJbFvCEct3uzu04ZoL"}, 
-    { "name": "Friday", "image": "Surprise!", "song": "Surprise!", "songlink": "Surprise!"}
-    // { "name": "Friday", "image": "https://i.scdn.co/image/ab67616d00001e02563151cc3a0528d8228998c8", "song": "Taylor Swift - Red", "songlink": "https://open.spotify.com/album/6x9s2ObPdpATZgrwxsk9c0"}
+    { "name": "Monday", "image": "https://www.artnews.com/wp-content/uploads/2020/04/strokes.jpg?w=682", "song": "DojaCat - Say So", "songlink": "https://www.youtube.com/watch?v=QYh6mYIJG2Y"},
+    { "name": "Tuesday", "image": "https://www.artnews.com/wp-content/uploads/2020/04/71bOXHJKHL._SL1400_-1.jpg", "song": "Pharrell Williams - Happy", "songlink": "https://www.youtube.com/watch?v=QYh6mYIJG2Y"}, 
+    { "name": "Wednesday", "image": "https://www.artnews.com/wp-content/uploads/2020/04/aretha.jpg", "song": "Harry Hudson - Yellow Lights", "songlink": "https://www.youtube.com/watch?v=QYh6mYIJG2Y"}, 
+    { "name": "Thursday", "image": "https://www.artnews.com/wp-content/uploads/2020/04/talking-heads.png", "song": "Katy Perry - Part of Me", "songlink": "https://www.youtube.com/watch?v=QYh6mYIJG2Y"}, 
+    { "name": "Friday", "image": "https://www.artnews.com/wp-content/uploads/2020/04/NYC-Peech-Boys.jpg", "song": "Taylor Swift - Back to December", "songlink": "https://www.youtube.com/watch?v=QYh6mYIJG2Y"}
   ]
-
+  
+ 
+  let x = document.cookie;   
+  let decodeCookie = decodeURIComponent(x); 
+  let ca= decodeCookie.split(';'); 
+  let cookieDict = getCookieDict(ca);   
+  for(var i in day){  
+    let e = day[i]; 
+    let weekday = e.name;     
+    if(weekday in cookieDict){
+      let info = cookieDict[weekday]; 
+      e.songlink = info['url'];  
+      e.image = info['image']; 
+      e.song = info['artist'] + " - "+info['name']; 
+    }
+  }
+  function getCookieDict(cList){
+    var dict = {}; 
+    for(let i = 0; i < cList.length; i++){
+      var set = cList[i]; 
+      var s = set.split("="); 
+      var d = s[0]; 
+      var info = JSON.parse(s[1]); 
+      dict[d] = info; 
+    }
+    return dict; 
+  }
 
   var element = [];
-    for (var i in day){
-      element.push((<Col span={4.8}>
-    <div onClick={redirectPage}> 
-    <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={<img alt={day[i].song} src={day[i].image}/>}
-      >
-        <Meta title={day[i].name} description={day[i].song}/>
-      </Card></div></Col>));
+  for (var i in day){
+    element.push((<Col span={4.8}>
+  <div onClick={event => window.location.href=day[i].songlink}> 
+  <Card
+      hoverable
+      style={{ width: 240 }}
+      cover={<img alt={day[i].song} src={day[i].image}/>
     }
+    >
+      <Meta title={day[i].name} description={day[i].song} />
+    </Card></div></Col>));
+  }
+  const [el, setEl] = useState(element); 
 
-  
-  function redirectPage() { 
-    let newPage = x;
-    day[0].song = y;
-    console.log(day[0].song);
-    window.location.href= newPage;
+  function updateSong(){
+    const d = new Date().toLocaleString('default', {weekday: 'long'});
+    const today = d.replace(" ", ""); 
+    let x = document.cookie;   
+    console.log(x); 
+    let decodeCookie = decodeURIComponent(x); 
+    let ca= decodeCookie.split(';'); 
+    let song = getInfo(today, ca);  
+    for(var i in day){  
+      let el = day[i]; 
+      let weekday = el.name;     
+      if(el.name == today){
+        el.songlink = song['url'];  
+        el.image = song['image']; 
+        el.song = song['artist'] + " - "+song['name']; 
+      }
+    } 
+    resetCom(); 
+  }
+
+  function resetCom (){
+  var element = [];
+  for (var i in day){
+    element.push((<Col span={4.8}>
+  <div onClick={event => window.location.href=day[i].songlink}> 
+  <Card
+      hoverable
+      style={{ width: 240 }}
+      cover={<img alt={day[i].song} src={day[i].image}/>}
+    >
+      <Meta title={day[i].name} description={day[i].song} />
+    </Card></div></Col>));
+  }
+  console.log(element); 
+  setEl(element); 
+ 
+  // const row = document.createElement("row");
+  // row.nodeValue = element;   
+  // row.setAttribute("id", "songs"); 
+  // row.setAttribute("gutter", 16); 
+  // const p = document.getElementById("songs"); 
+  // p.replaceWith(row); 
+    // var newEl = document.createElement("row");  
+    // newEl.setAttribute("id", "songs"); 
+    // newEl.setAttribute("gutter", 16); 
+    // newEl.innerHTML = {element};   
+    // var replace= document.getElementById ("h1");  
+    // replace.replaceChild(newEl, replace.childNode[0]); 
+  }
+
+  function getInfo(day, cookieList){
+    for(let i = 0; i < cookieList.length; i++){
+      let c= cookieList[i];  
+      if(c.indexOf(day)==0){
+        var found = c.substring(day.length+1, c.length);
+        found = JSON.parse(found); 
+        return found; 
+      }
+    }
+    return ""; 
   }
 
 
@@ -60,9 +130,9 @@ function App() {
        <h1 className="tunein-logo">
           TuneIn
         </h1>
-      <header className="App-header">
-        <Row gutter={16}>
-          {element}
+      <header className="App-header" >
+        <Row gutter={16} >
+          {el}
         </Row>
         <div className = 'day'>
 
@@ -71,24 +141,27 @@ function App() {
         <input className= "input-style" id ="feeling" />
         <div className="App-button"> 
           <button className="unstyled-button" onClick = {genSongClicked}>New Song! </button>
+          <button className = "unstyled-button" onClick = {updateSong} >Update</button>
         </div>
+        
         
       </header>
     </div>
   );
 }
 
-const token = "BQAJ1TgbwXvUGos21ZyGNEmv21jb9eBtwoXHJzX_4Q5se9tYuxb3PvK2FTh2BXAUaWRz7ic0C7jYDPPGLh4EFnE_OX6AwiQguXV47S6X4EuuEs8YkwLAZlCULEFUmAduYondleKzGpZ5bzDUxQdvLgk1AEaM";
+const token = "BQCljMrkZKQt1Uf8JVPXuxhuihhmjAAQumoO7EClfrjfcWZXcoexbcffwNN-2xXS78J8V5rD9-RGeffyfdPwk8D5ZBLPMyUIXtcaQrbN6Yisd7UxF1UUDeNpl5yb-OdiKz1owq2oo6QXUSBfe8QPGCQ1PNNm9EJbcjhcBwzBFVQIGgotqqP3V1nsP7Y";
 
 function genSongClicked() {
-  const dayToday = checkDay();
+  // const dayToday = checkDay();
+  const dayToday = new Date().toLocaleString('default', {weekday: 'long'});
   const word = document.getElementById('feeling').value;
- if (dayToday != false){
-    genSong(word, dayToday);
-   } 
-    else {
-    alert("A song has already been generated for today.");
-  }
+  genSong(word, dayToday); 
+  // if (dayToday != false){
+  //   genSong(word, dayToday);
+  // } else {
+  //   alert("A song has already been generated for today.");
+  // }
 
 
  }
@@ -116,48 +189,13 @@ function genSong(word, dayToday) {
         url: curTrack.external_urls.spotify,
         image: curTrack.album.images[0].url
       };
-      document.cookie = dayToday + "=" + JSON.stringify(curTrackJson);
-      x = curTrackJson.url;
-      y = curTrackJson.artist;
-      console.log(x);
-      console.log(y);
+      // var c=dayToday + "=" + curTrack.external_urls.spotify + ";"; 
+      // document.cookie = c; 
+      
+      document.cookie = dayToday + "=" + JSON.stringify(curTrackJson) + ";"; 
     });
     return;
   }
 
-  // data.tracks.items[0].external_urls.spotify
-  // document.cookie = dayToday + "=starships"
-  // console.log(document.cookie)
-  // return
-  // const fetchSong = async() => {
-  //   const response = await fetch('https://api.spotify.com/v1/search?q=' + word + '&type=track&limit=10', {
-  //     method: 'GET',
-  //     headers: {'Authorization' : 'Bearer ' + token}
-  //   })
-  //   .then(response => response.json())
-  //   .then((data) => {
-      
-  //      return data.tracks.items[0].external_urls.spotify;
-  //   });
-  //   const {song} = await response.json(); 
-  //   this.setState({
-  //     song 
-  //   }); 
-    
-
-  // }; 
-  // fetchSong(); 
-  
-  
-
-    // const choice = Math.floor(Math.random() * data.tracks.items.length);
-    // document.cookie = dayToday + "=" + data.tracks.items[choice].name;
-    // const newDiv = document.createElement("div"); 
-    // const newTag = '<a href= ' + song + '>Click Me</a>'; 
-    // newDiv.innerHTML = newTag;   
-    // const currentDiv = document.getElementById("newsong"); 
-    // let container = document.querySelector('div'); 
-    // let head = container.querySelector('header'); 
-    // container.insertBefore(newDiv,currentDiv ); 
 
 export default App;
